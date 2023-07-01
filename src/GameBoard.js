0271ec94c19455bbc9f62ec5a52a3b05223ddf6f
ed55567 <edblade555@gmail.com>
2023-06-30 20:25:17 -0400

    added check functions for winner columns

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -19,6 +19,34 @@ const GameBoard = () => {
         setBoard(newBoard);
     };
 
+    //check for a winner
+    const checkForWinner = (player) => {
+        //check rows
+     for (let row = 0; row < 6; row++) {
+         for (let col = 0; col < 4; col++) {
+             if (board[row][col] === player && 
+                board[row][col + 1] === player && 
+                board[row][col + 2] === player && 
+                board[row][col + 3] === player
+                ) {
+                 return true;
+             }
+         }
+     }
+
+     //check columns
+     for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 7; col++) {
+                if (board[row][col] === player &&
+                    board[row + 1][col] === player &&
+                    board[row + 2][col] === player &&
+                    board[row + 3][col] === player
+                ) {
+                    return true;
+                }
+            }
+        }
+
     return (
         <div>
             {/*TODO: Render the game board */}
