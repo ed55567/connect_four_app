bdfc644021f5ded3e45ace5e5fd64e6db316913b
ed55567 <edblade555@gmail.com>
2023-07-01 12:56:01 -0400

    made work with the oval wrapper

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Oval from './Oval';
 
 const Cell = ({ value, onClick }) => {
-  const circleImage = value === 1 ? 'circle-x.png' : 'circle-o.png';
+  const [isDropped, setIsDropped] = useState(false);
+
+  const handleCellClick = () => {
+    if (value === 0) {
+      setIsDropped(true);
+      onClick();
+    }
+  };
 
   return (
-    <div className="cell" onClick={onClick}>
-      {value !== 0 && <img src={circleImage} alt={value === 1 ? 'X' : 'O'} />}
+    <div className={`cell ${isDropped ? 'dropped' : ''}`} onClick={handleCellClick}>
+      {value !== 0 && (
+        <div className="oval-wrapper">
+          <Oval width={80} height={80} fill={value === 1 ? 'red' : 'yellow'} />
+        </div>
+      )}
     </div>
   );
 };
