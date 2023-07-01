cb3497161cfb160ac6cf027206dfaf66c53a0285
ed55567 <edblade555@gmail.com>
2023-06-30 20:16:27 -0400

    added initalizeboard and updateBoard functions

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -4,6 +4,20 @@ const GameBoard = () => {
     const [board, setBoard] = useState([]);
 
     // TODO: implement the game board logic
+    const initializeBoard = () => {
+        const newBoard = [];
+        for (let row = 0; row < 6; row++) {
+            newBoard.push(null);
+        }
+        return newBoard;
+    };
+
+    //Update the board state based on the player's move
+    const updateBoard = (row, column) => {
+        const newBoard = [...board];
+        newBoard[row][col] = currentPlayer;
+        setBoard(newBoard);
+    };
 
     return (
         <div>
