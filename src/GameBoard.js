9ee7b8e9d3b950f8d0544a6d3fc79ab0360c9508
ed55567 <edblade555@gmail.com>
2023-06-30 20:10:24 -0400

    added the GameBoard logic

---
+++ src/GameBoard.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const GameBoard = () => {
+    const [board, setBoard] = useState([]);
+
+    // TODO: implement the game board logic
+
+    return (
+        <div>
+            {/*TODO: Render the game board */}
+        </div>  
+    );
+};
+
+export default GameBoard;
